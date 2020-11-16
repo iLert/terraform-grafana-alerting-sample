4ba7d8bd4f6132d11c7c3af4cb05ba9e0d0fd2ee
yacut <roman.rogozhnikov@gmail.com>
2020-11-16 13:24:20 +0100

    add escalation policy and user

---
+++ ilert.tf
@@ -0,0 +1,86 @@
+resource "ilert_user" "this" {
+  email      = "example@example.com"
+  username   = "example"
+  first_name = "example"
+  last_name  = "example"
+
+  subscribed_incident_update_states             = ["ACCEPTED", "ESCALATED", "RESOLVED"]
+  subscribed_incident_update_notification_types = ["EMAIL"]
+
+  mobile {
+    region_code = "DE"
+    number      = "+4915231062570"
+  }
+
+  high_priority_notification_preference {
+    method = "EMAIL"
+    delay  = 0
+  }
+
+  high_priority_notification_preference {
+    method = "VOICE_MOBILE"
+    delay  = 0
+  }
+
+  high_priority_notification_preference {
+    method = "SMS"
+    delay  = 5
+  }
+
+  low_priority_notification_preference {
+    method = "EMAIL"
+    delay  = 0
+  }
+
+  on_call_notification_preference {
+    method     = "EMAIL"
+    before_min = 60
+  }
+}
+
+resource "ilert_escalation_policy" "grafana" {
+  name = "Grafana Policy"
+
+  escalation_rule {
+    escalation_timeout = 0
+    user               = ilert_user.this.id
+  }
+}
+
+resource "ilert_alert_source" "grafana" {
+  name                   = "Grafana Integration"
+  integration_type       = "GRAFANA"
+  escalation_policy      = ilert_escalation_policy.grafana.id
+  incident_priority_rule = "HIGH_DURING_SUPPORT_HOURS"
+
+  support_hours {
+    timezone = "Europe/Berlin"
+
+    support_days {
+      monday {
+        start = "08:00"
+        end   = "17:00"
+      }
+
+      tuesday {
+        start = "08:00"
+        end   = "17:00"
+      }
+
+      wednesday {
+        start = "08:00"
+        end   = "17:00"
+      }
+
+      thursday {
+        start = "08:00"
+        end   = "17:00"
+      }
+
+      friday {
+        start = "08:00"
+        end   = "17:00"
+      }
+    }
+  }
+}
